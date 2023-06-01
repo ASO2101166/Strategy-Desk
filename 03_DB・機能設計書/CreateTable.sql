0ecf1356b3b4f90d24dd77e405ca6e818d4e1c88
Saito14 <2101171@s.asojuku.ac.jp>
2023-06-01 09:59:24 +0900

    Update CreateTable.sql

--- 03_DB・機能設計書/CreateTable.sql
+++ 03_DB・機能設計書/CreateTable.sql
@@ -10,7 +10,7 @@ CREATE TABLE users
 CREATE TABLE boards
 (board_id               INT, --掲示板ID
  board_title            VARCHAR(255) NOT NULL, --タイトル
- board_tag              CHAR NOT NULL, --タグ
+ board_tag              VARCHAR(255) NOT NULL, --タグ
  initial_comment        TEXT NOT NULL, --初期コメント
  user_id                INT NOT NULL, --会員番号
  PRIMARY KEY (board_id),
@@ -24,7 +24,8 @@ CREATE TABLE comments
  like_count             INT NOT NULL DEFAULT 0, --高評価数
  not_like_count         INT NOT NULL DEFAULT 0, --低評価数
  fixed_comment          BOOLEAN NOT NULL, --固定コメント
- parent_comment_id      INT NOT NULL, --親コメントID
+ comment_data           DATE NOT NULL, --投稿日付
+ parent_comment_id      INT, --親コメントID
  user_id                INT NOT NULL, --会員番号
  PRIMARY KEY (board_id,comment_id),
  FOREIGN KEY (board_id) REFERENCES boards(board_id),
@@ -40,7 +41,7 @@ CREATE TABLE maps
 -- マップ部品テーブル作成 --
 CREATE TABLE map_parts
 (map_parts_id           INT, --マップ部品ID
- map_parts_image        BLOB NOT NULL, --マップ部品画像
+ map_parts_image        VARCHAR(255) NOT NULL, --マップ部品画像
  PRIMARY KEY (map_parts_id)
 );
 -- アンケートテーブル作成 --
@@ -50,11 +51,21 @@ CREATE TABLE questionaires
  questionary_all_votes  INT NOT NULL DEFAULT 0, --全体投票数
  PRIMARY KEY (questionary_id)
 );
+-- アンケート詳細テーブル作成 --
 CREATE TABLE questionary_details
 (questionary_id         INT, --アンケートID
  questionary_detail_id  INT, --アンケート詳細ID
- questionary_detail     VARCHAR(255) NOT NULL,
- questionary_votes      INT NOT NULL DEFAULT 0,
+ questionary_detail     VARCHAR(255) NOT NULL, --アンケート詳細
+ questionary_votes      INT NOT NULL DEFAULT 0, --投票数
  PRIMARY KEY (questionary_id,questionary_detail_id),
  FOREIGN KEY (questionary_id) REFERENCES questionaires(questionary_id)
 );
+-- アンケート投票テーブル作成 --
+CREATE TABLE questionary_
+(questionary_id         INT, --アンケートID
+ user_id                INT, --会員番号
+ PRIMARY KEY (questionary_id,user_id),
+ FOREIGN KEY (questionary_id) REFERENCES questionaires(questionary_id),
+ FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+COMMIT;
