cc3d51159d50549e56f1afbdb0ceda01997a8f7e
志水 <2101166@s.asojuku.ac.jp>
2023-06-06 23:33:10 +0900

    Update CreateTable.sql

--- 03_DB・機能設計書/CreateTable.sql
+++ 03_DB・機能設計書/CreateTable.sql
@@ -1,38 +1,44 @@
 START TRANSACTION;
 -- ユーザーテーブル作成 --
 CREATE TABLE users
-(user_id                INT AUTO_INCREMENT, --会員番号
- user_name              VARCHAR(255) NOT NULL, --ユーザー名
- password               VARCHAR(255) NOT NULL, --パスワード
+(user_id                INT AUTO_INCREMENT, -- 会員番号
+ user_name              VARCHAR(255) NOT NULL, -- ユーザー名
+ password               VARCHAR(255) NOT NULL, -- パスワード
  PRIMARY KEY (user_id)
 );
 -- 掲示板テーブルを作成 --
 CREATE TABLE boards
-(board_id               INT, --掲示板ID
- board_title            VARCHAR(255) NOT NULL, --タイトル
- user_id                INT NOT NULL, --会員番号
+(board_id               INT, -- 掲示板ID
+ board_title            VARCHAR(255) NOT NULL, -- タイトル
+ user_id                INT NOT NULL, -- 会員番号
  PRIMARY KEY (board_id),
  FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 -- タグテーブルを作成 --
 CREATE TABLE tags
-(board_id               INT, --掲示板ID
- tag_id                 INT, --タイトル
- tag_name               VARCHAR(255) NOT NULL, --タグ名
+(board_id               INT, -- 掲示板ID
+ tag_id                 INT, -- タイトル
+ tag_name               VARCHAR(255) NOT NULL, -- タグ名
  PRIMARY KEY (board_id, tag_id),
  FOREIGN KEY (board_id) REFERENCES boards(board_id)
 );
+-- マップテーブル作成 --
+CREATE TABLE maps
+(map_id                 INT, -- マップID
+ map_image              BLOB NOT NULL, -- マップ画像
+ PRIMARY KEY (map_id)
+);
 -- コメントテーブルを作成 --
 CREATE TABLE comments
-(board_id               INT, --掲示板ID
- comment_id             INT, --コメントID
- comment_content        TEXT NOT NULL, --コメント内容
- fixed_comment          BOOLEAN NOT NULL DEFAULT 0, --固定コメント
- comment_data           DATE NOT NULL, --投稿日付
- map_id                 INT, --マップID
- parent_board_id        INT, --親掲示板ID
- parent_comment_id      INT, --親コメントID
- user_id                INT NOT NULL, --会員番号
+(board_id               INT, -- 掲示板ID
+ comment_id             INT, -- コメントID
+ comment_content        TEXT NOT NULL, -- コメント内容
+ fixed_comment          BOOLEAN NOT NULL DEFAULT 0, -- 固定コメント
+ comment_data           DATE NOT NULL, -- 投稿日付
+ map_id                 INT, -- マップID
+ parent_board_id        INT, -- 親掲示板ID
+ parent_comment_id      INT, -- 親コメントID
+ user_id                INT NOT NULL, -- 会員番号
  PRIMARY KEY (board_id,comment_id),
  FOREIGN KEY (board_id) REFERENCES boards(board_id),
  FOREIGN KEY (map_id) REFERENCES maps(map_id),
@@ -41,43 +47,37 @@ CREATE TABLE comments
 );
 -- コメント評価テーブル作成 --
 CREATE TABLE comment_evaluations
-(board_id               INT, --掲示板ID
- comment_id             INT, --コメントID
- user_id                INT, --会員番号
- evaluation             BOOLEAN NOT NULL, --評価
+(board_id               INT, -- 掲示板ID
+ comment_id             INT, -- コメントID
+ user_id                INT, -- 会員番号
+ evaluation             BOOLEAN NOT NULL, -- 評価
  PRIMARY KEY (board_id,comment_id,user_id),
  FOREIGN KEY (board_id,comment_id) REFERENCES comments(board_id,comment_id),
  FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
--- マップテーブル作成 --
-CREATE TABLE maps
-(map_id                 INT, --マップID
- map_image              BLOB NOT NULL, --マップ画像
- PRIMARY KEY (map_id)
-);
 -- アンケートテーブル作成 --
 CREATE TABLE questionaires
-(board_id               INT, --掲示板ID
- questionary_id         INT, --アンケートID
- questionary_title      VARCHAR(255) NOT NULL, --題名
+(board_id               INT, -- 掲示板ID
+ questionary_id         INT, -- アンケートID
+ questionary_title      VARCHAR(255) NOT NULL, -- 題名
  PRIMARY KEY (board_id, questionary_id),
  FOREIGN KEY (board_id) REFERENCES boards(board_id)
 );
 -- アンケート詳細テーブル作成 --
 CREATE TABLE questionary_details
-(board_id               INT, --掲示板ID
- questionary_id         INT, --アンケートID
- questionary_detail_id  INT, --アンケート詳細ID
- questionary_detail     VARCHAR(255) NOT NULL, --アンケート詳細
- questionary_votes      INT NOT NULL DEFAULT 0, --投票数
+(board_id               INT, -- 掲示板ID
+ questionary_id         INT, -- アンケートID
+ questionary_detail_id  INT, -- アンケート詳細ID
+ questionary_detail     VARCHAR(255) NOT NULL, -- アンケート詳細
+ questionary_votes      INT NOT NULL DEFAULT 0, -- 投票数
  PRIMARY KEY (board_id, questionary_id, questionary_detail_id),
  FOREIGN KEY (board_id, questionary_id) REFERENCES questionaires(board_id, questionary_id)
 );
 -- アンケート投票テーブル作成 --
 CREATE TABLE questionary_votes
-(board_id               INT, --掲示板ID
- questionary_id         INT, --アンケートID
- user_id                INT, --会員番号
+(board_id               INT, -- 掲示板ID
+ questionary_id         INT, -- アンケートID
+ user_id                INT, -- 会員番号
  PRIMARY KEY (board_id, questionary_id, user_id),
  FOREIGN KEY (board_id, questionary_id) REFERENCES questionaires(board_id, questionary_id),
  FOREIGN KEY (user_id) REFERENCES users(user_id)
