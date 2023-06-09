b46721a74fff6c667c30996b176e5c0f27db961f
Saito14 <2101171@s.asojuku.ac.jp>
2023-06-09 11:00:48 +0900

    修正

--- 04_ソースコード/バックエンド/Dbconect.php
+++ 04_ソースコード/バックエンド/Dbconect.php
@@ -1,5 +1,5 @@
 <?php
-private function dbConnect(){
+public function dbConnect(){
     $pdo = new PDO('mysql:host=localhost;dbname=Strategy_Desk;charset=utf8','webuser','abccsd2');
     return $pdo;
 }
--- 04_ソースコード/バックエンド/UserSelect.php
+++ 04_ソースコード/バックエンド/UserSelect.php
@@ -1,9 +1,13 @@
 <?php
     require_once 'Dbconect.php';
-    $pdo = dbConnect();
+    $dbcon = new Dbconect();
+    $pdo = $dbcon->dbConnect();
     $sql = "SELECT * FROM user_table WHERE user_id = ?";
     $ps = $pdo->prepare($sql);
-    $ps->bindValue(1,$id,PDO::PARAM_INT);
+    $ps->bindValue(1,$_POST['id'],PDO::PARAM_INT);
     $ps->execute();
     $searchArray=$ps->fetchAll();
+    if(!empty($searchArray)){
+        
+    }  
 ?>
