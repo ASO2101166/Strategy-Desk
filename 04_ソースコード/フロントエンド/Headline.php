2cfe042c69c17faa1a0b4e8313a02c993a41a67a
aso2101174 <2101174@s.asojuku.ac.jp>
2023-06-16 10:41:27 +0900

    Update Headline.php

--- 04_ソースコード/フロントエンド/Headline.php
+++ 04_ソースコード/フロントエンド/Headline.php
@@ -2,17 +2,30 @@
 <html lang="ja">
     <head>
     <meta charset="UTF-8">
-    <title>ヘッター画面</title>
+    <title>ヘッダー画面</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
     <meta name="description" content="ここにサイト説明を入れます">
     <link rel="stylesheet" href="">
-    </head>
-    <body>
-        <header>
-            <h1 class="title">タイトル</h1>
-            <nav class="nav">
-                <ul class="">
-        <script src=""></script>
+</head>
+<body>
+    <header>
+        <nav>
+          <ul>
+            <li><a href="#">ホーム</a></li>
+            <li><a href="#">戻る</a></li>
+            <li><a href="#">検索</a></li>
+            <li><a href="#">ログイン</a></li>
+          </ul>
+        </nav>
+      </header>
+      
+      <div class="content">
+        <h2>ヘッダー画面</h2> 
+      </div>
+      
+      <footer>
+        <!-- フッターの内容 -->
+      </footer>
     </body>
-</html>
+    </html>
