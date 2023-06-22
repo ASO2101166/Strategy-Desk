3aa87e42f8fac3bfe8a63553b4b352c1562b9e2a
志水 <2101166@s.asojuku.ac.jp>
2023-06-22 12:10:48 +0900

    Board.php(HTML)

--- 04_ソースコード/frontend/Board.php
+++ 04_ソースコード/frontend/Board.php
@@ -22,31 +22,64 @@
                     <div class="plus"></div>
                 </div>
                 <!-- アンケートを開くタグ -->
-                <div class="questionary_tab">
+                <div id="questionary_tab">
                     <div>アンケート</div>
-                    <div>
-                        
-                    </div>
+                    <div class="plus"></div>
                 </div>
-                <div>a</div>
             </div>
             <!-- 中央エリア -->
             <div class="comment_area">
                 <div class="comment">
                     <div class="comment_info">
                         <div class="comment_number">1</div>
-                        <div class="comment_user"></div>
-                        <div class="comment_date"></div>
+                        <div class="comment_user">志水太郎</div>
+                        <div class="comment_date">2023/06/22 10:12</div>
                     </div>
                     <div class="comment_content">
-                        ・
+                        <div class="comment_text">・こんにちは</div>
                     </div>
                 </div>
+                <div class="comment">
+                    <div class="comment_info">
+                        <div class="comment_number">2</div>
+                        <div class="comment_user">杉本太郎</div>
+                        <div class="comment_date">2023/06/22 10:17</div>
+                    </div>
+                    <div class="comment_content">
+                        <div class="comment_text">・こんばんは</div>
+                    </div>
+                </div>
+                <div class="comment">
+                    <div class="comment_info">
+                        <div class="comment_number">3</div>
+                        <div class="comment_user">髙橋太郎</div>
+                        <div class="comment_date">2023/06/22 10:22</div>
+                    </div>
+                    <div class="comment_content">
+                        <image src="test.png" alt></image>
+                    </div>
+                </div>
+                <!-- かさ増し -->
                 <div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div><div>a</div>
             </div>
             <!-- 右エリア -->
             <div class="fixed_comment_area">
-                <div>a</div>
+                <div id="fixed_toggle_button">
+                    <div class="arrow_icon">
+                        <i class="bi bi-arrow-up-left-circle-fill"></i>
+                    </div>
+                    <div>OFF</div>
+                </div>
+                <div class="fixed_comment">
+                    <div class="comment_info">
+                        <div class="comment_number">1</div>
+                        <div class="comment_user">志水太郎</div>
+                        <div class="comment_date">2023/06/22 10:12</div>
+                    </div>
+                    <div class="comment_content">
+                        <div class="comment_text">・こんにちは</div>
+                    </div>
+                </div>
             </div>
 
         </div>
