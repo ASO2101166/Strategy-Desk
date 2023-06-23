995174f2f8eeb47bf0bbdea0cc989b7b6bb8b182
aso2101174 <2101174@s.asojuku.ac.jp>
2023-06-23 12:12:45 +0900

    ヘッダー

---
+++ 04_ソースコード/frontend/Questionnaire.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+    <meta charset="UTF-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <meta name="description" content="ここにサイト説明を入れます">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+    <div><class="">
