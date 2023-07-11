772d7984910ea818195e6f64dba716adfb95c231
Saito14 <2101171@s.asojuku.ac.jp>
2023-07-11 11:34:55 +0900

    微修正

--- 04_ソースコード/backend/BoardHistorySelect.php
+++ 04_ソースコード/backend/BoardHistorySelect.php
@@ -1,14 +1,16 @@
 <?php
 public class BoardHistorySelect{
-public function boardHistorySelect(){
-session_start();
-if(isset($_COOKIE['history'])){
-    $his_id = explode(',',$_COOKIE['history']);
-    /*foreach($his_id as $row){
-        echo '<li><a href="'.$row.'"></a></li>';
-    }*/
-    return $his_id;
-}
-}
+    public function boardHistorySelect(){
+        session_start();
+        if(isset($_COOKIE['history'])){
+            $his_id = explode(',',$_COOKIE['history']);
+            /*foreach($his_id as $row){
+                echo '<li><a href="'.$row.'"></a></li>';
+            }*/
+            return $his_id;
+    }   else{
+            return null;
+        }
+    }
 }
 ?>
