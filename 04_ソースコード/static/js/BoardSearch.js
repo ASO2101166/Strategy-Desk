62bb2875ddd2fdda9810fccb761b754b8685cf64
志水 <2101166@s.asojuku.ac.jp>
2023-07-12 19:12:45 +0900

    検索機能紐づけ完了

--- 04_ソースコード/static/js/BoardSearch.js
+++ 04_ソースコード/static/js/BoardSearch.js
@@ -1,8 +1,9 @@
 function search_board(e){
+    let search_form = document.getElementById("search_form").value;
     const data = {
-        
+        search_form : search_form
     }
-    fetch('', {
+    fetch('../backend/BoardSelect.php', {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
@@ -14,7 +15,24 @@ function search_board(e){
         return response.json()
     })
     .then(res => {
-        
+        let search_main = document.getElementById("search_main");
+        while(search_main.firstChild){
+            search_main.removeChild(search_main.firstChild);
+        }
+        let clone_search_board_area = document.getElementById("clone_search_board_area");
+        res.forEach(function(data){
+            let clone_area = clone_search_board_area.cloneNode(true);
+            clone_area.hidden = false;
+            clone_area.children[0].children[1].children[0].innerHTML = data[1];
+            clone_area.children[0].children[0].value = data[0];
+            for(let i = 0; i < data[2].length; i++){
+                let tag_name = document.createElement("div");
+                tag_name.className = "search_tag";
+                tag_name.innerHTML = data[2][i];
+                clone_area.children[0].children[1].children[1].appendChild(tag_name);
+            }
+            search_main.appendChild(clone_area);
+        })
     })
     .catch(error => {
         console.log(error); // エラー表示
