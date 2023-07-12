7c36eae6fe78fc5362e1de8bee3fb96ae6f77c4c
aso2101174 <2101174@s.asojuku.ac.jp>
2023-07-12 09:34:07 +0900

    Questionnphp

--- 04_ソースコード/frontend/Questionnaire.php
+++ 04_ソースコード/frontend/Questionnaire.php
@@ -10,11 +10,25 @@
     <link rel="stylesheet" href="../static/css/Questionnaire.css">
     </head>
     <body>
+
+
         <div class="questionary_area">
             <!-- この中↓を編集 -->
+            <input type="text" class="my-textbox">
+            <button type="button" class="btn">⇗</button>
 
+            <div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333; background-color: #ffff99;">
+            </div>
 
+            <div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333; background-color: #ffff99;">
+            </div>
 
+            <div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333; background-color: #ffff99;">
+            </div>
+
+            <div style="padding: 10px; margin-bottom: 5Spx; border: 1px solid #333333;">
+            マップ
+            </div>
             <!-- この中↑を編集 -->
         </div>
         <script src=""></script>
